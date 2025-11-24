44907d7e455a68da7ffa1b778accac730e8bfec6
Jonas Söderholm <jonas.soderholm89@gmail.com>
2025-11-24 14:02:54 +0100

    chore: make landing page SSR and remove landingpage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
-import LandingPage from "@/components/LandingPage";
+import HeroVideo from "@/components/HeroVideo";
+import LandingTabs from "@/components/LandingTabs";
 
-export default function Home() {
+export default function Page() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <main className="w-full min-h-screen bg-slate-200">
+      <div className="w-full flex justify-center md:px-3">
+        <HeroVideo />
+      </div>
+
+      <LandingTabs />
+    </main>
   );
 }
